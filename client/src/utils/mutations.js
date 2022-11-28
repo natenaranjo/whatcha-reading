36af5943db813531290c04addef2ac2559ee4f4b
Nate Naranjo <naranjo_nathaniel@live.com>
2022-11-28 14:42:00 -0600

    Mutation js need to update saveBook and removeBook bookData variables.  SearchBooks file add useMutation useQueury savebook to use mutation and update the try to fetch to google api

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,62 +1,61 @@
-import { gql } from "@apollo/client";
+import gql from 'graphql-tag';
 
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-        email
-      }
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($input: BookInput) {
-    saveBook(input: $input) {
-      _id
-      username
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        image
-        link
-        title
-        description
-      }
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+        _id
+        username
+        email
+        savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
     }
-  }
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeFriend($bookId: String!) {
-    removeBook(bookId: $bookId) {
-      _id
-      username
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        image
-        link
-        title
-        description
-      }
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image  
+                title
+                description
+                link
+            }
+        }
     }
-  }
 `;
